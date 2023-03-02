6f15cf0ea0bcdd3f3e920ad7061958174616e4a0
Kamila Pantoja IPDEC <kamila.pantoja@ipdec.org>
2023-03-02 09:37:38 -0400

    [feat]: criação do carrinho

--- src/cart/entities/cart.entity.ts
+++ src/cart/entities/cart.entity.ts
@@ -7,21 +7,21 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
-
 @Entity({ name: 'cart' })
 export class CartEntity {
   @PrimaryGeneratedColumn('rowid')
   id: number;
-
   @Column({ name: 'user_id', nullable: false })
   userId: number;
+
+  @Column({ name: 'active', nullable: false })
+  active: boolean;
 
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
-
   @OneToMany(() => CartProductEntity, (cartProduct) => cartProduct.cart)
   cartProduct?: CartProductEntity[];
 }
