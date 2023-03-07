63a770647ef1369753457596ea6bc659dff49364
Kamila Pantoja IPDEC <kamila.pantoja@ipdec.org>
2023-03-07 15:18:20 -0400

    [test]:testes unitarios dos controllers parte 2

---
+++ src/product/__tests__/product.controller.spec.ts
@@ -0,0 +1,71 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ProductController } from '../product.controller';
+import { ProductService } from '../product.service';
+import { productMock } from '../../product/__Mocks__/product.mock';
+import { createProductMock } from '../__Mocks__/create-product.mock';
+import { returnDeleteMock } from '../../product/__Mocks__/return-delete.mock';
+import { updateProductMock } from '../../product/__Mocks__/update-product.mock';
+
+describe('ProductController', () => {
+  let controller: ProductController;
+  let productService: ProductService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: ProductService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue([productMock]),
+            createProduct: jest.fn().mockResolvedValue(productMock),
+            updateProduct: jest.fn().mockResolvedValue(productMock),
+            deleteProduct: jest.fn().mockResolvedValue(returnDeleteMock),
+          },
+        },
+      ],
+      controllers: [ProductController],
+    }).compile();
+
+    controller = module.get<ProductController>(ProductController);
+    productService = module.get<ProductService>(ProductService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+    expect(productService).toBeDefined();
+  });
+
+  it('should return returnProduct in findAll', async () => {
+    const products = await controller.findAll();
+
+    expect(products).toEqual([
+      {
+        id: productMock.id,
+        name: productMock.name,
+        price: productMock.price,
+        image: productMock.image,
+      },
+    ]);
+  });
+
+  it('should return productEntity in createProduct', async () => {
+    const product = await controller.createProduct(createProductMock);
+
+    expect(product).toEqual(productMock);
+  });
+
+  it('should return returnDelete in deleteProduct', async () => {
+    const product = await controller.deleteProduct(productMock.id);
+
+    expect(product).toEqual(returnDeleteMock);
+  });
+
+  it('should return ProductEntity in updateProduct', async () => {
+    const product = await controller.updateProduct(
+      updateProductMock,
+      productMock.id,
+    );
+
+    expect(product).toEqual(productMock);
+  });
+});
