288def8764d542108456a59e6b4df8b5da518408
Kamila Pantoja IPDEC <kamila.pantoja@ipdec.org>
2023-04-03 08:32:05 -0400

    [feat]:Aula 71-Criação de testes unitarios de buscar pedidos

--- src/order/__tests__/order.controller.spec.ts
+++ src/order/__tests__/order.controller.spec.ts
@@ -15,6 +15,7 @@ describe('OrderController', () => {
           useValue: {
             createOrder: jest.fn().mockResolvedValue(orderMock),
             findOrdersByUserId: jest.fn().mockResolvedValue([orderMock]),
+            findAllOrders: jest.fn().mockResolvedValue([orderMock]),
           },
         },
       ],
@@ -32,5 +33,18 @@ describe('OrderController', () => {
     const orders = await controller.findOrdersByUserId(userEntityMock.id);
 
     expect(orders).toEqual([orderMock]);
+  });
+
+  it('should return orders in findAllOrders', async () => {
+    const spy = jest.spyOn(orderService, 'findAllOrders');
+    const orders = await controller.findAllOrders();
+
+    expect(orders).toEqual([
+      {
+        id: orderMock.id,
+        date: orderMock.date.toString(),
+      },
+    ]);
+    expect(spy.mock.calls.length).toEqual(1);
   });
 });
--- src/order/dtos/return-order.dto.ts
+++ src/order/dtos/return-order.dto.ts
@@ -1,4 +1,4 @@
-import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { ReturnUserDto } from '../../user/dtos/returnUser.dto';
 import { OrderEntity } from '../entities/orders.entity';
 
 export class ReturnOderDTO {
